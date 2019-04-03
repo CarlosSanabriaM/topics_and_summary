44d6beb0e2a56e5ddbe75d12de8de813f8122127
Carlos Sanabria <carlos.sanabria@accenture.com>
2019-04-03 10:40:39 +0200

    Added functions to save and load functions on disk, using dill.

--- cnn-sentence-classification/cnn-sentence-classification-project/utils.py
+++ cnn-sentence-classification/cnn-sentence-classification-project/utils.py
@@ -2,6 +2,8 @@ import pickle
 from datetime import datetime
 from os import path
 
+import dill
+
 RANDOM_STATE = 100
 # This python module (utils.py) must be in the root folder of the project. If not, the following PATH won't be OK.
 PROJECT_ROOT_PATH = path.dirname(path.abspath(__file__))
@@ -35,7 +37,7 @@ def now_as_str():
 
 def save_obj_to_disk(obj, name):
     """
-    Stores an object in disk, using pickle.
+    Stores an object on disk, using pickle.
     :param obj: Object to be stored in disk.
     :param name: Name of the pickle file to be created in saved-models/objects/ and
     where the object info will be stored.
@@ -48,9 +50,32 @@ def save_obj_to_disk(obj, name):
 def load_obj_from_disk(name):
     """
     Loads a saved object from disk, using pickle.
-    :param name: Name of the pickle file stores in saved-models/objects/.
+    :param name: Name of the pickle file stored in saved-models/objects/.
     :return: The object load from disk.
     """
     obj_path = get_abspath_from_project_root('saved-models/objects/{}.pickle'.format(name))
     with open(obj_path, 'rb') as f:
         return pickle.load(f)
+
+
+def save_func_to_disk(func, name):
+    """
+    Stores a function on disk, using dill.
+    :param func: Function to be stored in disk.
+    :param name: Name of the dill file to be created in saved-models/funcs/ and
+    where the object info will be stored.
+    """
+    obj_path = get_abspath_from_project_root('saved-models/funcs/{}.dill'.format(name))
+    with open(obj_path, 'wb') as f:
+        dill.dump(func, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_func_from_disk(name):
+    """
+    Loads a saved func from disk, using dill.
+    :param name: Name of the dill file stored in saved-models/funcs/.
+    :return: The object load from disk.
+    """
+    func_path = get_abspath_from_project_root('saved-models/funcs/{}.dill'.format(name))
+    with open(func_path, 'rb') as f:
+        return dill.load(f)
