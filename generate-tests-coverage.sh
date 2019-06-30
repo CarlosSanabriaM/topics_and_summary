61d50d30ec56d88cdfedbe544491aeeb5e14185e
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-30 14:20:59 +0200

    Added topics_and_summary package to the generate-tests-coverage.sh file.

--- generate-tests-coverage.sh
+++ generate-tests-coverage.sh
@@ -4,6 +4,6 @@
 coverage erase
 # Generate the .coverage file with the tests coverage
 # All the Python packages under the topics_and_summary folder whose test coverage want to be known must be added to the --source option, separated by commas
-coverage run --branch --source=topics_and_summary/datasets,topics_and_summary/models,topics_and_summary/preprocessing topics_and_summary/tests/run_all_tests.py
+coverage run --branch --source=topics_and_summary,topics_and_summary/datasets,topics_and_summary/models,topics_and_summary/preprocessing topics_and_summary/tests/run_all_tests.py
 # Generate the coverage.xml file using the .coverage file
 coverage xml -i
